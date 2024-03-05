d6f1674ae247dfb08b8a8f83afded1c9ce038bfa
SanthoshKatada <santhoshchampkatada@gmail.com>
2024-03-05 19:27:14 +0530

    Third Commit

--- lua/santhosh/core/options.lua
+++ lua/santhosh/core/options.lua
@@ -3,3 +3,28 @@ local opt = vim.opt
 -- Appearence
 opt.number = true
 opt.relativenumber = true
+
+-- tabing 
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- Line Wrap
+opt.wrap = false
+
+-- Search 
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.background = "dark"
+
+-- Clipboard
+opt.clipboard:append("unnamedplus")
+
+-- Slpit Windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
